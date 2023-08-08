3abe8e95c9597c4cd462d496031f22c3a539b030
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2023-08-08 16:17:13 +0900

    update: app.bsky.embed.externalを必ず作るように変更

--- src/postBluesky.ts
+++ src/postBluesky.ts
@@ -21,37 +21,41 @@ export default async ({
   mimeType?: string;
   image?: Uint8Array;
 }) => {
+  const thumb = await (async () => {
+    if (image instanceof Uint8Array && typeof mimeType === 'string') {
+      // 画像をアップロード
+      const uploadedImage = await agent.uploadBlob(image, {
+        encoding: mimeType,
+      });
+
+      // 投稿オブジェクトに画像を追加
+      return {
+        $type: 'blob',
+        ref: {
+          $link: uploadedImage.data.blob.ref.toString(),
+        },
+        mimeType: uploadedImage.data.blob.mimeType,
+        size: uploadedImage.data.blob.size,
+      };
+    }
+  })();
+
   const postObj: Partial<AtprotoAPI.AppBskyFeedPost.Record> &
     Omit<AtprotoAPI.AppBskyFeedPost.Record, 'createdAt'> = {
     $type: 'app.bsky.feed.post',
     text: rt.text,
     facets: rt.facets,
-  };
-
-  if (image instanceof Uint8Array && typeof mimeType === 'string') {
-    // 画像をアップロード
-    const uploadedImage = await agent.uploadBlob(image, {
-      encoding: mimeType,
-    });
-
-    // 投稿オブジェクトに画像を追加
-    postObj.embed = {
+    embed: {
       $type: 'app.bsky.embed.external',
       external: {
         uri: link,
-        thumb: {
-          $type: 'blob',
-          ref: {
-            $link: uploadedImage.data.blob.ref.toString(),
-          },
-          mimeType: uploadedImage.data.blob.mimeType,
-          size: uploadedImage.data.blob.size,
-        },
         title,
         description,
+        thumb,
       },
-    };
-  }
+    },
+  };
+
   console.log(JSON.stringify(postObj, null, 2));
   await agent.post(postObj);
   console.log('post to Bluesky');
